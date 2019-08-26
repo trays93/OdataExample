60c6ec7e4ed136c21ea0b1dbbcf3577ed9eb9b6e
Lovas Bálint <lovasb93@gmail.com>
2019-08-26 21:52:17 +0200

    Entitás törlése a halmazból (HTTP DELETE)

--- ProductService/Controllers/ProductsController.cs
+++ ProductService/Controllers/ProductsController.cs
@@ -112,5 +112,19 @@ namespace ProductService.Controllers
 
             return Updated(update);
         }
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var product = await db.Products.FindAsync(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
     }
 }
