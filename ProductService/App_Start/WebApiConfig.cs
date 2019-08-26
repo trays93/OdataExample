c86f025f414a53cace28749c490058ff36741fe6
Lovas Bálint <lovasb93@gmail.com>
2019-08-26 21:15:49 +0200

    OData végpont konfigurálása

--- ProductService/App_Start/WebApiConfig.cs
+++ ProductService/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using ProductService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,16 +12,12 @@ namespace ProductService
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
         }
     }
 }
