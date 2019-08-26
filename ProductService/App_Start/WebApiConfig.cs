73c6ceff2584d4b91190380a2640d2f7b676601c
Lovas Bálint <lovasb93@gmail.com>
2019-08-26 21:21:13 +0200

    ProductsController osztály hozzáadva

--- ProductService/App_Start/WebApiConfig.cs
+++ ProductService/App_Start/WebApiConfig.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNet.OData.Extensions;
 using ProductService.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
 
 namespace ProductService
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.OData;
+using ProductService.Models;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ProductService.Controllers
+{
+    public class ProductsController : ODataController
+    {
+        private ProductsContext db = new ProductsContext();
+
+        private bool ProductExists(int key)
+        {
+            return db.Products.Any(p => p.ID == key);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
