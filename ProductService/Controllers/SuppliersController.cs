b4929eede60e064e7d699697ca8f835593db7107
Lovas Bálint <lovasb93@gmail.com>
2019-08-30 19:43:23 +0200

    SuppliersController hozzáadva

---
+++ ProductService/Controllers/SuppliersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.OData;
+using ProductService.Models;
+using System.Linq;
+
+namespace ProductService.Controllers
+{
+    public class SuppliersController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+
+        private bool SupplierExists(int key)
+        {
+            return db.Suppliers.Any(s => s.ID == key);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
