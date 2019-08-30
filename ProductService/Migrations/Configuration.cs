8b4cbb01e8665380792644350e3b1967f821bf11
Lovas Bálint <lovasb93@gmail.com>
2019-08-30 23:45:40 +0200

    Példaadatok hozzáadása az adatbázishoz.

--- ProductService/Migrations/Configuration.cs
+++ ProductService/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace ProductService.Migrations
 {
+    using ProductService.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +19,53 @@ namespace ProductService.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+
+            // Add suppliers.
+            Supplier[] suppliers = new Supplier[]
+            {
+                new Supplier()
+                {
+                    Name = "Beszállító #1"
+                },
+                new Supplier()
+                {
+                    Name = "Beszállító #2"
+                }
+            };
+            context.Suppliers.AddOrUpdate(x => x.ID, suppliers);
+            context.SaveChanges();
+
+            // Add products.
+            Product[] products = new Product[]
+            {
+                new Product()
+                {
+                    Name = "Termék #1",
+                    Price = 5.95M,
+                    Category = "Kategória #1",
+                    Supplier = context.Suppliers.
+                        SingleOrDefault(s => s.Name == "Beszállító #1")
+                },
+                new Product()
+                {
+                    Name = "Termék #2",
+                    Price = 6.73M,
+                    Category = "Kategória #2",
+                    Supplier = context.Suppliers.
+                        SingleOrDefault(s => s.Name == "Beszállító #1")
+                },
+                new Product()
+                {
+                    Name = "Termék #3",
+                    Price = 4.72M,
+                    Category = "Kategória #1",
+                    Supplier = context.Suppliers.
+                        SingleOrDefault(s => s.Name == "Beszállító #2")
+                }
+            };
+            context.Products.AddOrUpdate(x => x.ID, products);
+            context.SaveChanges();
         }
     }
 }
