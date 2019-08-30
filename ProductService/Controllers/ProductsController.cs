300ce64ed4b8d07c6ee7082b093e4461851d19fa
Lovas Bálint <lovasb93@gmail.com>
2019-08-30 23:02:17 +0200

    Rate metódus implementálva

--- ProductService/Controllers/ProductsController.cs
+++ ProductService/Controllers/ProductsController.cs
@@ -187,5 +187,40 @@ namespace ProductService.Controllers
 
             return StatusCode(HttpStatusCode.NoContent);
         }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Rate([FromODataUri] int key,
+            ODataActionParameters parameters)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            int rating = (int)parameters["Rating"];
+            db.Ratings.Add(new ProductRating
+            {
+                ProductID = key,
+                Rating = rating
+            });
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
     }
 }
