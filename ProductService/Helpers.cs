72e79e1152211fd5d0eb196c4e5a97092a121878
Lovas Bálint <lovasb93@gmail.com>
2019-08-30 22:12:20 +0200

    Helpers osztály hozzáadva

---
+++ ProductService/Helpers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData.UriParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace ProductService
+{
+    public class Helpers
+    {
+        public static TKey GetKeyFromUri<TKey>(HttpRequestMessage request, Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri");
+            }
+
+            var urlHelper = request.GetUrlHelper() ?? new UrlHelper(request);
+
+            string serviceRoot = urlHelper.CreateODataLink(
+                request.ODataProperties().RouteName,
+                request.GetPathHandler(),
+                new List<ODataPathSegment>());
+            var odataPath = request.GetPathHandler().Parse(
+                serviceRoot, uri.LocalPath, request.GetRequestContainer());
+
+            var keySegment = odataPath.Segments.OfType<KeySegment>().FirstOrDefault();
+            if (keySegment == null)
+            {
+                throw new InvalidOperationException("The link does not contain a key.");
+            }
+
+            return (TKey)keySegment.Keys.First().Value;
+        }
+    }
+}
