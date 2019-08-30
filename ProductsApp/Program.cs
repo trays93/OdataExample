aaf1d6dbbd05c4eb4fb5522a05127169d8a25195
Lovas Bálint <lovasb93@gmail.com>
2019-08-31 01:26:39 +0200

    Supplierek és Productok lekérdezése és listásása a kliensalkalmazásban.

--- ProductsApp/Program.cs
+++ ProductsApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProductService;
 
 namespace ProductsApp
 {
@@ -12,10 +13,51 @@ namespace ProductsApp
         {
             Console.WriteLine("***** ProductsApp kliens *****");
 
-            // TODO: alkalmazás logika
+            Uri uri = new Uri("https://localhost:44387/");
 
+            ListSuppliers(uri);
+            ListProducts(uri);
+            
             Console.WriteLine("Nyomj entert a folytatáshoz");
             Console.ReadLine();
+        }
+
+        private static void ListSuppliers(Uri uri)
+        {
+            var container = new Container(uri);
+            var suppliers = container.Suppliers.Expand("Products").Execute();
+            Console.WriteLine("\nBeszállítók:");
+            foreach (var supplier in suppliers)
+            {
+                Console.WriteLine($"\t{supplier.Name}");
+                if (supplier.Products != null)
+                {
+                    foreach (var product in supplier.Products)
+                    {
+                        Console.WriteLine($"\t\t{product.Name}");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void ListProducts(Uri uri)
+        {
+            var container = new Container(uri);
+            var products = container.Products.Expand("Supplier").Execute();
+            Console.WriteLine("\nTermékek:");
+            foreach (var product in products)
+            {
+                if (product.Supplier != null)
+                {
+                    Console.WriteLine($"\t{product.Name}, {product.Price}, {product.Supplier.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"\t{product.Name}, {product.Price}");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
