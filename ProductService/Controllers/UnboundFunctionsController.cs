03d13ca38d6f604003a413d5fc82812b98aae152
Lovas Bálint <lovasb93@gmail.com>
2019-08-30 23:15:11 +0200

    UnboundFunctionsController osztály hozzáadva

---
+++ ProductService/Controllers/UnboundFunctionsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using System.Web.Http;
+
+namespace ProductService.Controllers
+{
+    public class UnboundFunctionsController : ODataController
+    {
+        [HttpGet]
+        [ODataRoute("GetSalesTaxRate(PostalCode={postalCode})")]
+        public IHttpActionResult GetSalesTaxRate([FromODataUri] int postalCode)
+        {
+            double rate = 5.6; // Use a fake number for the sample.
+            return Ok(rate);
+        }
+    }
+}
